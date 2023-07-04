048bc2f242c811eb95493f66a0a8dc62247e42ae
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-07-04 10:39:36 +0000

    add new contact added to navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<div className="container"> 
+			<nav className="navbar navbar-white bg-white mb-3">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1"></span>
 				</Link>
-			</div>
-		</nav>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-success">Add new contact</button>
+					</Link>
+				</div>
+			</nav>	
+		</div>
 	);
 };
